963107cf03f439a124bb8e9a9034047fbf38a0a8
thanhvk <thanhvk.w3s@gmail.com>
2022-10-18 15:10:08 +0700

    render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,8 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
@@ -27,6 +28,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
